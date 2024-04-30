21729846d5e7b7173b568699873efd7c30a5caa0
codezen879 <sanchitcodecrafter@gmail.com>
2024-04-30 16:44:51 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                LEARN BLOCKCHAINa <u><b>NOW! </b></u>
+                LEARN BLOCKCHAINas<u><b>NOW! </b></u>
               </a>
             </div>
           </main>
